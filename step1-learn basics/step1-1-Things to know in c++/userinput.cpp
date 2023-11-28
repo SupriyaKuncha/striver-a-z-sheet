cbd2378461525fafaef117dd82ef60e5a586e76e
supriya kuncha <kunchasupriya181@gmail.com>
2023-11-28 09:45:43 +0530

    adding files

---
+++ step1-learn basics/step1-1-Things to know in c++/userinput.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Write your code here
+    char input;
+    cin >> input;
+    if (input >= 65 && input <= 90)
+    {
+        cout << "1" << endl;
+    }
+    else if (input >= 97 && input <= 122)
+    {
+        cout << "0" << endl;
+    }
+    else
+    {
+        cout << "-1" << endl;
+    }
+}
